b00e0572b2c5d12cfe8c1a769a509baef130b624
Devika-Sajeesh <devikasajeesh26@gmail.com>
2026-02-20 09:38:36 +0530

    initial commit

--- src/RunnableThread.java
+++ src/RunnableThread.java
@@ -1,7 +1,7 @@
 class MyRunnable implements Runnable {
     @Override
     public void run(){
-        for(int i=0;i<5;i++){
+        for(int i=1;i<=5;i++){
             System.out.print(i+" ");
         }
     }
